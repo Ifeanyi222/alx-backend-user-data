690c666090dafbb30863c4f1f12c0a7a59b526e5
Ifeanyi222 <linusify@ymail.com>
2023-05-25 12:06:28 +0000

    task5 done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,7 +2,7 @@
 """Encrypting passwords """
 import bcrypt
 
-def hash_password(password)->str():
+def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
